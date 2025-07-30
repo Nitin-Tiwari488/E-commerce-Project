159882af74b3c65bd099ba74860c3de2ce2d8d23
Nitin Tiwari <nitintiwari0058@gmail.com>
2025-07-31 01:03:35 +0530

    fix: Add new Netlify URL to CORS origins for ecommerceproject1707.netlify.app

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://funny-fairy-a307db.netlify.app',
+    'https://ecommerceproject1707.netlify.app',
     'https://netlify.app',
     'https://vercel.app'
   ],
